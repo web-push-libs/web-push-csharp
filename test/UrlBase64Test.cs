ee7f25a1d2054e4ae240c8460ff575702563aa3f
Cory Thompson <cthompson@busivid.com>
2016-12-21 14:07:12 +1100

    Added project for testing.

---
+++ test/UrlBase64Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebPush.Util;
+
+namespace WebPush.Test
+{
+    [TestClass]
+    public class UrlBase64Test
+    {
+        [TestMethod]
+        public void TestBase64UrlDecode()
+        {
+            byte[] expected = new byte[3] {181, 235, 45};
+            byte[] actual = UrlBase64.Decode(@"test");
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        public void TestBase64UrlEncode()
+        {
+            string expected = @"test";
+            string actual = UrlBase64.Encode(new byte[3] {181, 235, 45});
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
