8540da59ea41544737112974ef8246ca9b1d2f9b
Cory Thompson <cthompson@busivid.com>
2016-12-27 21:03:10 +1100

    Added unit tests for GenerateVapidKeys

---
+++ test/VapidHelperTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using WebPush;
+using WebPush.Util;
+
+namespace WebPush.Test
+{
+    [TestFixture]
+    public class VapidHelperTest
+    {
+        [Test]
+        public void TestGenerateVapidKeys()
+        {
+            VapidDetails keys = VapidHelper.GenerateVapidKeys();
+            byte[] publicKey = UrlBase64.Decode(keys.PublicKey);
+            byte[] privateKey = UrlBase64.Decode(keys.PrivateKey);
+
+            Assert.AreEqual(32, privateKey.Length);
+            Assert.AreEqual(65, publicKey.Length);
+        }
+
+        [Test]
+        public void TestGenerateVapidKeysNoCache()
+        {
+            VapidDetails keys1 = VapidHelper.GenerateVapidKeys();
+            VapidDetails keys2 = VapidHelper.GenerateVapidKeys();
+
+            Assert.AreNotEqual(keys1.PublicKey, keys2.PublicKey);
+            Assert.AreNotEqual(keys1.PrivateKey, keys2.PrivateKey);
+        }
+    }
+}
