f444aa81e08b238ab04bd8e6178ecead0ae4bb4b
Cory Thompson <public@coryjthompson.com>
2021-02-28 10:05:59 +1100

    #71 202 HTTP response causes WebPushException

--- WebPush/WebPushClient.cs
+++ WebPush/WebPushClient.cs
@@ -383,8 +383,7 @@ namespace WebPush
         private static void HandleResponse(HttpResponseMessage response, PushSubscription subscription)
         {
             // Successful
-            if (response.StatusCode == HttpStatusCode.Created ||
-                response.StatusCode == HttpStatusCode.Accepted)
+            if (response.IsSuccessStatusCode)
             {
                 return;
             }
